d4b37eb16314b471218e9854cd6a75c19739e099
Christian Canonigo <28989015+cjcanonigo@users.noreply.github.com>
2019-02-11 22:56:07 -0800

    v1

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # logIP
+
+## Purpose
+
+To check for IP changes (on a home network).
+
+This will create 2 files, a currIP.txt and a histIP.txt.
+
+currIP.txt will keep a single row containing the IPv4 external address of the current network.
+histIP.txt will keep a history of timestamps + IPv4 based on whenever the script is run.
+
+Lastly, it will connect to an authorized slack channel via OAuth token and send a message if the IP has changed.
---
+++ logIP.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+
+import subprocess
+import datetime as dt
+from slackclient import SlackClient
+import sys
+
+# Log Vars
+currIP = 'currIP.txt'
+histIP = 'histIP.txt'
+
+# Slack Vars
+sameIP = False
+
+# Set Credentials
+if len(sys.argv) > 1:
+    slackToken = sys.argv[1]
+    slackChannel = sys.argv[2]
+
+# Run IP Command
+cmd = ['dig', '@resolver1.opendns.com', 'ANY', 'myip.opendns.com', '+short']
+result = subprocess.run(cmd, stdout=subprocess.PIPE)
+ip = result.stdout.decode('utf-8')
+
+# Build Timestamp
+now = dt.datetime.now().strftime("%Y-%m-%d %H:%M:%S | ")
+
+# Check if IP changed
+with open(currIP, 'r+') as f:
+    first_line = f.readline()
+    if first_line == ip:
+        sameIP = True
+    else:
+        print('ip has changed from {}'.format(first_line))
+    # always overwrite currIP file
+    f.seek(0)
+    f.write(ip)
+    f.truncate()
+
+# Save the IP to a log
+with open(histIP, "a") as log:
+    line = now + ip
+    log.write(line)
+    print(line)
+
+# Slack Vars
+slackMsg = 'Current Home IP: ' + ip
+
+if not sameIP:
+    # Run Slack if data has changed
+    sc = SlackClient(slackToken)
+    sc.api_call("chat.postMessage", channel=slackChannel, text=slackMsg)
